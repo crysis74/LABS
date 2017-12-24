b4eb90a4a716e1e58cef935db6d329eabf9431e4
crysis74 <jekabt2@mail.ru>
2017-12-25 01:19:13 +0300

    gegre

--- src/events/Dialog.java
+++ src/events/Dialog.java
@@ -18,24 +18,60 @@ public class Dialog implements Event  {
         this.weather = weather;
     }
     public String tellStory(){
-        StringBuilder dialog = new StringBuilder("Между " + speaker1.sayName() + " и " + speaker2.sayName() +
-                " произошел следующий диалог: \n");
-        switch (weather){
-            case FOG: dialog.append("Герои не видели даже свои руки из-за густого тумана.\n"); break;
-            case SUN: dialog.append("Хорошо, что здесь была хорошая солнечная погода.\n"); break;
-            case DARK: dialog.append("Герои были испуганы, ведь на улице было темно.\n"); break;
-        }
-        for(int i = 0; i < dialogText.size();i++){
-            if(i % 2 == 0) {
-                dialog.append(speaker1.sayName() + ": ");
+        class DialogMaker {
+            private int number;
+            private AbstractHero speaker1;
+            private AbstractHero speaker2;
+            private Weather weather;
+
+            DialogMaker(AbstractHero speaker1, AbstractHero speaker2,Weather weather) {
+                this.speaker1 = speaker1;
+                this.speaker2 = speaker2;
+                number = 0;
+                this.weather = weather;
+
             }
-            else {
-                dialog.append(speaker2.sayName() + ": ");
+
+            StringBuilder dialog = new StringBuilder("Между " + speaker1.sayName() + " и " + speaker2.sayName() +
+                    " произошел следующий диалог: \n");
+
+            public void showWeather() {
+                switch (weather) {
+                    case FOG:
+                        dialog.append("Герои не видели даже свои руки из-за густого тумана.\n");
+                        break;
+                    case SUN:
+                        dialog.append("Хорошо, что здесь была хорошая солнечная погода.\n");
+                        break;
+                    case DARK:
+                        dialog.append("Герои были испуганы, ведь на улице было темно.\n");
+                        break;
+                }
+
             }
-            dialog.append(dialogText.get(i) + "\n");
+
+            public void addPhraze(String phrase, int i) {
+
+                    if (number % 2 == 0) {
+                        dialog.append(speaker1.sayName() + ": ");
+                    } else {
+                        dialog.append(speaker2.sayName() + ": ");
+                    }
+                    dialog.append(dialogText.get(i) + "\n");
+                    number++;
+                }
+
+            public String getDialog() {
+                return dialog.toString();
+            }
         }
-        return dialog.toString();
-    }
+        DialogMaker dia = new DialogMaker(speaker1, speaker2, weather);
+        dia.showWeather();
+        for (int i=0;i < dialogText.size(); i++){
+            dia.addPhraze(dialogText.get(i),i);
+        }
+            return dia.getDialog();
+        }
 
     public String toString(){
         return "диалог между " + speaker1.sayName() + " и " + speaker2.sayName();
